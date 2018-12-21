a2fc8adfab3b12d5cfe743916b4807955fdabcd3
Rahul Narain <rahul.narain@gmail.com>
2018-12-21 20:26:01 +0530

    Changed all submodule URLs to HTTPS, added Python to dependencies

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 # Tested on fresh install of Ubuntu 18.04 LTS
 # If you want parallel compilation, run `MAKEFLAGS="-j$(nproc)" ./install.sh`
 
-sudo apt-get -qy install git build-essential cmake libboost-dev libboost-filesystem-dev libboost-serialization-dev libeigen3-dev libglew-dev libglfw3-dev libpng-dev
+echo 'Installing dependencies via apt. You may be asked to enter your password.'
+sudo apt-get -qy install git build-essential cmake libboost-dev libboost-filesystem-dev libboost-serialization-dev libeigen3-dev libglew-dev libglfw3-dev libpng-dev python
 git submodule update --init --recursive
 mkdir -p build
 pushd build
