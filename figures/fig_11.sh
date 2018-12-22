804d45621c6630bae3d147e99e8d4e4608dfe2a4
Rahul Narain <rahul.narain@gmail.com>
2018-12-22 14:22:07 +0530

    Fig 11 script fixed

--- figures/fig_11.sh
+++ figures/fig_11.sh
@@ -3,11 +3,13 @@
 # ensure predictable working directory
 cd "$(dirname "$0")"
 
+pushd ..
 for name in box_and_cone_0_{00,17,34,50};
 do
-    outdir=../output/figures/fig_11/$name
+    outdir=output/figures/fig_11/$name
     mkdir -p $outdir
-    ../build/apps/argus-cloth simulateoffline ../conf/$name.json $outdir
+    build/apps/argus-cloth simulateoffline conf/$name.json $outdir
 done
+popd
 
 pdflatex fig_11.tex
