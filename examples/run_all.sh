9eaf01437fb1ddbeb9b5b885feb506d803c34e72
Jie Li <lixx4611@umn.edu>
2019-04-05 00:04:28 -0500

    Modified run_all.sh

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -1,22 +1,22 @@
 echo "WARNING: Please be aware that running all the examples can take a few days (each character animation can take 5 to more than 10 hours, also depending on your computer specifications)."
 
-python examples/belt_high_tension.py -s 1 -d 0 -p performance.txt
-python examples/box_and_cone.py -s 1 -d 0 -p performance.txt
-python examples/cards_high_friction.py -s 1 -d 0 -p performance.txt
-python examples/cards_low_friction.py -s 1 -d 0 -p performance.txt
-python examples/drag_cloth.py -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 0 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 1 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 2 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 3 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 4 -s 1 -d 0 -p performance.txt
 
 # Following are character examples
-python examples/character/Arabesque-0-0.py -s 1 -d 0 -p performance.txt
-python examples/character/Arabesque-0-3.py -s 1 -d 0 -p performance.txt
-python examples/character/Clubbing-0-0.py -s 1 -d 0 -p performance.txt
-python examples/character/Clubbing-0-1.py -s 1 -d 0 -p performance.txt
-python examples/character/Clubbing-0-3.py -s 1 -d 0 -p performance.txt
-python examples/character/HipHop-0-0.py -s 1 -d 0 -p performance.txt
-python examples/character/HipHop-0-3.py -s 1 -d 0 -p performance.txt
-python examples/character/Shawl-0-3.py -s 1 -d 0 -p performance.txt
-python examples/character/Shawl-0-6.py -s 1 -d 0 -p performance.txt
-python examples/character/Twist-0-0.py -s 1 -d 0 -p performance.txt
-python examples/character/Twist-0-1.py -s 1 -d 0 -p performance.txt
-python examples/character/Twist-0-3.py -s 1 -d 0 -p performance.txt
-python examples/character/Twist-0-6.py -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 5 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 6 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 7 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 8 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 9 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 10 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 11 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 12 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 13 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 14 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 15 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 16 -s 1 -d 0 -p performance.txt
+python examples/argus_interface.py -e 17 -s 1 -d 0 -p performance.txt
